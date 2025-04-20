eef8b53ddb1a4e527c704eab765c14818de1dd51
lucmayor <mayorl@myumanitoba.ca>
2025-04-20 02:41:48 -0500

    update for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  // @ts-ignore
+  adapter: vercel(),
   integrations: [svelte()]
 });
